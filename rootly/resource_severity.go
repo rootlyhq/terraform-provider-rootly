32fbf584af153e1b6e6b708cd6480fbd74ac9d8d
Ninir <gauthier.wallet@gmail.com>
2022-03-08 22:18:30 -0800

    Update client + add working CRUD

--- rootly/resource_severity.go
+++ rootly/resource_severity.go
@@ -31,21 +31,23 @@ func resourceSeverity() *schema.Resource {
 				Type:        schema.TypeString,
 				Optional:    true,
 			},
+			"slug": {
+				Description: "The slug of the severity",
+				Type:        schema.TypeString,
+				Optional:    true,
+				Computed:    true,
+			},
 			"severity": {
 				Description: "The description of the severity",
 				Type:        schema.TypeString,
 				Optional:    true,
+				Default:     "medium",
 				ValidateFunc: validation.StringInSlice([]string{
 					"critical",
 					"high",
 					"medium",
 					"low",
 				}, false),
-			},
-			"color": {
-				Description: "The color of the severity",
-				Type:        schema.TypeString,
-				Optional:    true,
 			},
 		},
 	}
@@ -55,36 +57,46 @@ func resourceSeverityCreate(ctx context.Context, d *schema.ResourceData, meta in
 	c := meta.(*client.Client)
 
 	name := d.Get("name").(string)
+	severity := d.Get("severity").(string)
+
+	tflog.Trace(ctx, fmt.Sprintf("Creating Severity: %s", name))
 
 	s := &client.Severity{
-		Name: name,
+		Name:     name,
+		Severity: severity,
 	}
 
-	if description, ok := d.GetOk("description"); ok {
-		s.Description = description.(string)
+	if value, ok := d.GetOk("description"); ok {
+		s.Description = value.(string)
 	}
 
-	if color, ok := d.GetOk("color"); ok {
-		s.Color = color.(string)
+	if value, ok := d.GetOk("slug"); ok {
+		s.Slug = value.(string)
 	}
 
-	severity, err := c.CreateSeverity(s)
+	if value, ok := d.GetOk("severity"); ok {
+		s.Severity = value.(string)
+	}
+
+	res, err := c.CreateSeverity(s)
 	if err != nil {
 		return diag.Errorf("Error creating severity: %s", err.Error())
 	}
 
-	d.SetId(severity.ID)
-	tflog.Trace(ctx, "created a resource")
+	d.SetId(res.ID)
+	tflog.Trace(ctx, fmt.Sprintf("created a severity resource: %v (%s)", name, d.Id()))
 
 	return resourceSeverityRead(ctx, d, meta)
 }
 
 func resourceSeverityRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	c := meta.(*client.Client)
+	tflog.Trace(ctx, fmt.Sprintf("Reading Severity: %s", d.Id()))
 
 	severity, err := c.GetSeverity(d.Id())
 	if err != nil {
 		// In the case of a NotFoundError, it means the resource may have been removed upstream
+		// We just remove it from the state.
 		if _, ok := err.(client.NotFoundError); ok && !d.IsNewResource() {
 			tflog.Warn(ctx, fmt.Sprintf("Severity (%s) not found, removing from state", d.Id()))
 			d.SetId("")
@@ -95,20 +107,58 @@ func resourceSeverityRead(ctx context.Context, d *schema.ResourceData, meta inte
 	}
 
 	d.Set("name", severity.Name)
+	d.Set("severity", severity.Severity)
+	d.Set("description", severity.Description)
+	d.Set("slug", severity.Slug)
 
-	return diag.Errorf("not implemented")
+	return nil
 }
 
 func resourceSeverityUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	// use the meta value to retrieve your client from the provider configure method
-	// client := meta.(*apiClient)
+	c := meta.(*client.Client)
+	tflog.Trace(ctx, fmt.Sprintf("Updating Severity: %s", d.Id()))
 
-	return diag.Errorf("not implemented")
+	name := d.Get("name").(string)
+	severity := d.Get("severity").(string)
+
+	s := &client.Severity{
+		Name:     name,
+		Severity: severity,
+	}
+
+	if d.HasChange("description") {
+		s.Description = d.Get("description").(string)
+	}
+
+	if d.HasChange("severity") {
+		s.Severity = d.Get("severity").(string)
+	}
+
+	_, err := c.UpdateSeverity(d.Id(), s)
+	if err != nil {
+		return diag.Errorf("Error updating severity: %s", err.Error())
+	}
+
+	return resourceSeverityRead(ctx, d, meta)
 }
 
 func resourceSeverityDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	// use the meta value to retrieve your client from the provider configure method
-	// client := meta.(*apiClient)
+	c := meta.(*client.Client)
 
-	return diag.Errorf("not implemented")
+	err := c.DeleteSeverity(d.Id())
+	if err != nil {
+		// In the case of a NotFoundError, it means the resource may have been removed upstream.
+		// We just remove it from the state.
+		if _, ok := err.(client.NotFoundError); ok && !d.IsNewResource() {
+			tflog.Warn(ctx, fmt.Sprintf("Severity (%s) not found, removing from state", d.Id()))
+			d.SetId("")
+			return nil
+		}
+		return diag.Errorf("Error deleting severity: %s", err.Error())
+	}
+
+	d.SetId("")
+	tflog.Trace(ctx, "deleted a resource")
+
+	return nil
 }
--- rootly/resource_severity_test.go
+++ rootly/resource_severity_test.go
@@ -16,6 +16,15 @@ func TestAccResourceSeverity(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(
 						"rootly_severity.foo", "name", "mysev"),
+					resource.TestCheckResourceAttr("rootly_severity.foo", "severity", "medium"),
+				),
+			},
+			{
+				Config: testAccResourceSeverityUpdate,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("rootly_severity.foo", "name", "mysev2"),
+					resource.TestCheckResourceAttr("rootly_severity.foo", "severity", "high"),
+					resource.TestCheckResourceAttr("rootly_severity.foo", "description", "test description"),
 				),
 			},
 		},
@@ -25,5 +34,13 @@ func TestAccResourceSeverity(t *testing.T) {
 const testAccResourceSeverity = `
 resource "rootly_severity" "foo" {
   name = "mysev"
+}
+`
+
+const testAccResourceSeverityUpdate = `
+resource "rootly_severity" "foo" {
+  name        = "mysev2"
+  severity    = "high"
+  description = "test description"
 }
 `
