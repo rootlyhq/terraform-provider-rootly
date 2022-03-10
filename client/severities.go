52fbb744ff6bf20940f0672b0debcb56ab87eac2
Ninir <gauthier.wallet@gmail.com>
2022-03-09 22:24:51 -0800

    Cleanup + address NotFoundErrors

--- client/severities.go
+++ client/severities.go
@@ -2,7 +2,6 @@ package client
 
 import (
 	"bytes"
-	"fmt"
 	"github.com/google/jsonapi"
 	"github.com/pkg/errors"
 	rootlygo "github.com/rootlyhq/rootly-go"
@@ -43,7 +42,6 @@ func (c *Client) CreateSeverity(s *Severity) (*Severity, error) {
 	}
 	resp, err := c.Do(req)
 	if err != nil {
-		fmt.Println(err)
 		return nil, errors.Errorf("Failed to make request to create severity" + "\n\n" + buffer.String())
 	}
 
@@ -55,58 +53,58 @@ func (c *Client) CreateSeverity(s *Severity) (*Severity, error) {
 	return severity, nil
 }
 
-//func (c *Client) GetSeverity(id string) (*Severity, error) {
-//	req, err := http.NewRequest("GET", fmt.Sprintf("%s/%s", SeveritiesPath, id), nil)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	body, err := c.doRequest(req)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	severity := new(Severity)
-//	if err := jsonapi.UnmarshalPayload(bytes.NewReader(body), severity); err != nil {
-//		return nil, errors.Errorf("Error unmarshaling severity (creation): %s", err.Error())
-//	}
-//
-//	return severity, nil
-//}
-//
-//func (c *Client) UpdateSeverity(id string, s *Severity) (*Severity, error) {
-//	buffer := new(bytes.Buffer)
-//	if err := jsonapi.MarshalPayload(buffer, s); err != nil {
-//		return nil, errors.Errorf("Error marshaling severity (update): %s", err.Error())
-//	}
-//	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/%s", SeveritiesPath, id), buffer)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	body, err := c.doRequest(req)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	severity := new(Severity)
-//	if err := jsonapi.UnmarshalPayload(bytes.NewReader(body), severity); err != nil {
-//		return nil, errors.Errorf("Error unmarshaling severity (creation): %s", err.Error())
-//	}
-//
-//	return severity, nil
-//}
-//
-//func (c *Client) DeleteSeverity(id string) error {
-//	req, err := http.NewRequest("DELETE", fmt.Sprintf("%s/%s", SeveritiesPath, id), nil)
-//	if err != nil {
-//		return err
-//	}
-//
-//	_, err = c.doRequest(req)
-//	if err != nil {
-//		return err
-//	}
-//
-//	return nil
-//}
+func (c *Client) GetSeverity(id string) (*Severity, error) {
+	req, err := rootlygo.NewGetSeverityRequest(c.Rootly.Server, id)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.Do(req)
+	if err != nil {
+		return nil, errors.Errorf("Failed to make request to get severity: %s", id)
+	}
+
+	severity := new(Severity)
+	if err := jsonapi.UnmarshalPayload(resp.Body, severity); err != nil {
+		return nil, errors.Errorf("Error unmarshaling severity (read): %s", err.Error())
+	}
+
+	return severity, nil
+}
+
+func (c *Client) UpdateSeverity(id string, s *Severity) (*Severity, error) {
+	buffer := new(bytes.Buffer)
+	if err := jsonapi.MarshalPayload(buffer, s); err != nil {
+		return nil, errors.Errorf("Error marshaling severity (update): %s", err.Error())
+	}
+
+	req, err := rootlygo.NewUpdateSeverityRequestWithBody(c.Rootly.Server, id, c.ContentType, buffer)
+	if err != nil {
+		return nil, errors.Errorf("Error unmarshaling severity (update): %s", err.Error())
+	}
+	resp, err := c.Do(req)
+	if err != nil {
+		return nil, errors.Errorf("Failed to make request to update severity: %s", id)
+	}
+
+	severity := new(Severity)
+	if err := jsonapi.UnmarshalPayload(resp.Body, severity); err != nil {
+		return nil, errors.Errorf("Error unmarshaling severity (update): %s", err.Error())
+	}
+
+	return severity, nil
+}
+
+func (c *Client) DeleteSeverity(id string) error {
+	req, err := rootlygo.NewDeleteSeverityRequest(c.Rootly.Server, id)
+	if err != nil {
+		return errors.Errorf("Error unmarshaling severity (delete): %s", err.Error())
+	}
+
+	_, err = c.Do(req)
+	if err != nil {
+		return errors.Errorf("Failed to make request to delete severity: %s", id)
+	}
+
+	return nil
+}
