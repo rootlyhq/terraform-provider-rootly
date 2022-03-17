2de0e441b89e4e5f073232fec0c3f06a7aaf3056
Ninir <gauthier.wallet@gmail.com>
2022-03-16 20:21:39 -0700

    Add incident type resource + tests

---
+++ rootly/resource_incident_type_test.go
@@ -0,0 +1,46 @@
+package provider
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccResourceIncidentType(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceIncidentType,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("rootly_incident_type.foo", "name", "myincidenttyoe"),
+					resource.TestCheckResourceAttr("rootly_incident_type.foo", "description", ""),
+					resource.TestCheckResourceAttr("rootly_incident_type.foo", "color", "#047BF8"),
+				),
+			},
+			{
+				Config: testAccResourceIncidentTypeUpdate,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("rootly_incident_type.foo", "name", "myincidenttyoe2"),
+					resource.TestCheckResourceAttr("rootly_incident_type.foo", "description", "test description"),
+					resource.TestCheckResourceAttr("rootly_incident_type.foo", "color", "#203"),
+				),
+			},
+		},
+	})
+}
+
+const testAccResourceIncidentType = `
+resource "rootly_incident_type" "foo" {
+  name = "myincidenttyoe"
+}
+`
+
+const testAccResourceIncidentTypeUpdate = `
+resource "rootly_incident_type" "foo" {
+  name        = "myincidenttyoe2"
+  description = "test description"
+  color       = "#203"
+}
+`
