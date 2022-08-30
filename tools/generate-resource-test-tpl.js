f5ede87869958801d379a489d1c011817c12080f
Alex Mingoia <alex@alexmingoia.com>
2022-08-30 22:08:48 +0630

    Auto-generate (some) tests.

---
+++ tools/generate-resource-test-tpl.js
@@ -0,0 +1,80 @@
+const inflect = require('inflect')
+
+module.exports = (name, resourceSchema, requiredFields, pathIdField) => {
+	const namePlural = inflect.pluralize(name)
+	const nameCamel = inflect.camelize(name)
+	const nameCamelPlural = inflect.camelize(namePlural)
+
+return `package provider
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccResource${nameCamel}(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() {
+			testAccPreCheck(t)
+		},
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResource${nameCamel},
+			},
+			{
+				Config: testAccDataSource${nameCamel},
+			},
+		},
+	})
+}
+
+const testAccDataSource${nameCamel} = \`
+resource "rootly_${name}" "test" {
+	${testParams(name, resourceSchema, requiredFields || [])}
+}
+
+data "rootly_${name}" "test" {
+	depends_on = [rootly_${name}.test]
+}
+
+output "${name}" {
+	value = data.rootly_${name}.test.id
+}
+\`
+
+const testAccResource${nameCamel} = \`
+resource "rootly_${name}" "test" {
+	${testParams(name, resourceSchema, requiredFields || [])}
+}
+\`
+`
+}
+
+function testParams(name, schema, required) {
+	return (required).map((key) => {
+		let val = schema.properties[key].enum ? schema.properties[key].enum[0] : "test"
+		switch (schema.properties[key].type) {
+			case "boolean":
+				return `${key} = false`
+			case "string":
+				return `${key} = "${val}"`
+			case "number":
+				return `${key} = 1`
+			case "array":
+				if (schema.properties[key].items.type === "object" && schema.properties[key].items.properties.id) {
+					return `${key} {
+						id = "foo"
+						name = "bar"
+					}`
+				}
+				return `${key} = ["foo"]`
+			case "object":
+				return `${key} = {
+					id = "foo"
+					name = "bar"
+				}`
+		}
+	}).join('\n')
+}
---
+++ tools/generate-workflow-tpl.js
@@ -0,0 +1,296 @@
+const inflect = require('inflect');
+
+module.exports = (name, resourceSchema, requiredFields, taskParamsSchema) => {
+	const namePlural = inflect.pluralize(name)
+	const nameCamel = inflect.camelize(name)
+	const nameCamelPlural = inflect.camelize(namePlural)
+
+return `package provider
+
+import (
+	"context"
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/rootlyhq/terraform-provider-rootly/client"
+)
+
+func resource${nameCamel}() *schema.Resource{
+	return &schema.Resource{
+		CreateContext: resource${nameCamel}Create,
+		ReadContext: resource${nameCamel}Read,
+		UpdateContext: resource${nameCamel}Update,
+		DeleteContext: resource${nameCamel}Delete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		Schema: map[string]*schema.Schema{
+			${schemaFields(resourceSchema, requiredFields, taskParamsSchema)}
+		},
+	}
+}
+
+func resource${nameCamel}Create(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	c := meta.(*client.Client)
+
+	tflog.Trace(ctx, fmt.Sprintf("Creating ${nameCamel}"))
+
+	s := &client.Workflow{}
+
+	${createResourceFields(resourceSchema)}
+
+	res, err := c.CreateWorkflow(s)
+	if err != nil {
+		return diag.Errorf("Error creating ${name}: %s", err.Error())
+	}
+
+	d.SetId(res.ID)
+	tflog.Trace(ctx, fmt.Sprintf("created a ${name} resource: %s", d.Id()))
+
+	return resource${nameCamel}Read(ctx, d, meta)
+}
+
+func resource${nameCamel}Read(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	c := meta.(*client.Client)
+	tflog.Trace(ctx, fmt.Sprintf("Reading ${nameCamel}: %s", d.Id()))
+
+	item, err := c.GetWorkflow(d.Id())
+	if err != nil {
+		// In the case of a NotFoundError, it means the resource may have been removed upstream
+		// We just remove it from the state.
+		if _, ok := err.(client.NotFoundError); ok && !d.IsNewResource() {
+			tflog.Warn(ctx, fmt.Sprintf("${nameCamel} (%s) not found, removing from state", d.Id()))
+			d.SetId("")
+			return nil
+		}
+
+		return diag.Errorf("Error reading ${name}: %s", d.Id())
+	}
+
+	${setResourceFields(resourceSchema)}
+
+	return nil
+}
+
+func resource${nameCamel}Update(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	c := meta.(*client.Client)
+	tflog.Trace(ctx, fmt.Sprintf("Updating ${nameCamel}: %s", d.Id()))
+
+	s := &client.Workflow{}
+
+	${updateResourceFields(resourceSchema)}
+
+	_, err := c.UpdateWorkflow(d.Id(), s)
+	if err != nil {
+		return diag.Errorf("Error updating ${name}: %s", err.Error())
+	}
+
+	return resource${nameCamel}Read(ctx, d, meta)
+}
+
+func resource${nameCamel}Delete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	c := meta.(*client.Client)
+	tflog.Trace(ctx, fmt.Sprintf("Deleting ${nameCamel}: %s", d.Id()))
+
+	err := c.DeleteWorkflow(d.Id())
+	if err != nil {
+		// In the case of a NotFoundError, it means the resource may have been removed upstream.
+		// We just remove it from the state.
+		if _, ok := err.(client.NotFoundError); ok && !d.IsNewResource() {
+			tflog.Warn(ctx, fmt.Sprintf("${nameCamel} (%s) not found, removing from state", d.Id()))
+			d.SetId("")
+			return nil
+		}
+		return diag.Errorf("Error deleting ${name}: %s", err.Error())
+	}
+
+	d.SetId("")
+
+	return nil
+}
+`}
+
+function excludeDateFields(field) {
+	return field !== 'created_at' && field !== 'updated_at'
+}
+
+function setResourceFields(resourceSchema) {
+	return Object.keys(resourceSchema.properties).filter(excludeDateFields).map((field) => {
+		if (field === "trigger_params") {
+			return `
+        tps := make([]interface{}, 1, 1)
+        tps[0] = item.TriggerParams
+        d.Set("trigger_params", tps)
+			`
+		} else {
+			return `d.Set("${field}", item.${inflect.camelize(field)})`
+		}
+	}).join('\n  ')
+}
+
+function createResourceFields(resourceSchema) {
+	return Object.keys(resourceSchema.properties).filter(excludeDateFields).map((field) => {
+		const schema = resourceSchema.properties[field]
+		if (field === "trigger_params") {
+			return`  if value, ok := d.GetOkExists("${field}"); ok {
+			s.${inflect.camelize(field)} = value.(${jsonapiToGoType(schema.type)})
+	}`
+		} else {
+			return`  if value, ok := d.GetOkExists("${field}"); ok {
+			s.TriggerParams = value.([]interface{})[0].(map[string]interface{})
+	}`
+		}
+	}).join('\n  ')
+}
+
+function updateResourceFields(resourceSchema) {
+	return Object.keys(resourceSchema.properties).filter(excludeDateFields).map((field) => {
+		const schema = resourceSchema.properties[field]
+		if (field === "trigger_params") {
+			return `
+				if d.HasChange("trigger_params") {
+					tps := d.Get("trigger_params").([]interface{})
+					for _, tpsi := range tps {
+						s.TriggerParams = tpsi.(map[string]interface{})
+					}
+				}`
+		} else {
+			return`
+				if d.HasChange("${field}") {
+					s.${inflect.camelize(field)} = d.Get("${field}").(${jsonapiToGoType(schema.type)})
+				}`
+		}
+	}).join('\n  ')
+}
+
+function jsonapiToGoType(type) {
+	switch (type) {
+		case 'string':
+			return 'string'
+		case 'number':
+			return 'int'
+		case 'boolean':
+			return 'bool'
+		case 'array':
+			return '[]interface{}'
+		case 'object':
+			return 'interface{}'
+		default:
+			return 'string'
+	}
+}
+
+function schemaFields(resourceSchema, requiredFields, taskParamsSchema) {
+	return Object.keys(resourceSchema.properties).filter(excludeDateFields).map((field) => {
+		return schemaField(field, resourceSchema, requiredFields, taskParamsSchema)
+	}).join('\n')
+}
+
+function schemaField(name, resourceSchema, requiredFields, taskParamsSchema) {
+	const schema = resourceSchema.properties[name]
+	const optional = (requiredFields || []).indexOf(name) === -1 || schema.enum ? "true" : "false"
+	const required = (requiredFields || []).indexOf(name) === -1 || schema.enum ? "false" : "true"
+	const description = (schema.description || '').replace(/"/g, '\\"')
+	switch (schema.type) {
+		case 'string':
+			return `
+			"${name}": &schema.Schema{
+				Type: schema.TypeString,
+				${schema.enum ? `Default: "${schema.enum[0]}",` : `Computed: ${optional},`}
+				Required: ${required},
+				Optional: ${optional},
+				Description: "${description}",
+			},
+			`
+		case 'number':
+			return `
+			"${name}": &schema.Schema{
+				Type: schema.TypeInt,
+				Computed: ${optional},
+				Required: ${required},
+				Optional: ${optional},
+				Description: "${description}",
+			},
+			`
+		case 'boolean':
+			return `
+			"${name}": &schema.Schema{
+				Type: schema.TypeBool,
+				Computed: ${optional},
+				Required: ${required},
+				Optional: ${optional},
+				Description: "${description}",
+			},
+			`
+		case 'array':
+			if (schema.items && schema.items.type === "object") {
+				return `
+				"${name}": &schema.Schema{
+					Type: schema.TypeList,
+					Computed: ${optional},
+					Required: ${required},
+					Optional: ${optional},
+					Description: "${description}",
+					Elem: &schema.Resource{
+						Schema: map[string]*schema.Schema{
+							"id": &schema.Schema{
+								Type: schema.TypeString,
+								Required: true,
+							},
+							"name": &schema.Schema{
+								Type: schema.TypeString,
+								Required: true,
+							},
+						},
+					},
+				},
+				`
+			} else {
+				return `
+				"${name}": &schema.Schema{
+					Type: schema.TypeList,
+					Elem: &schema.Schema{
+						Type: schema.TypeString,
+					},
+					Computed: ${optional},
+					Required: ${required},
+					Optional: ${optional},
+					Description: "${description}",
+				},
+				`
+			}
+		case 'object':
+		default:
+			if (name === "trigger_params") {
+				return `
+				"${name}": &schema.Schema{
+					Type: schema.TypeList,
+					MinItems: 1,
+					MaxItems: 1,
+					Elem: &schema.Resource{
+						Schema: map[string]*schema.Schema{
+							${schemaFields(taskParamsSchema, taskParamsSchema.required)}
+						},
+					},
+					Computed: ${optional},
+					Required: ${required},
+					Optional: ${optional},
+					Description: "${description}",
+				},
+				`
+			}
+			return `
+			"${name}": &schema.Schema{
+				Type: schema.TypeMap,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+				Computed: ${optional},
+				Required: ${required},
+				Optional: ${optional},
+				Description: "${description}",
+			},
+			`
+	}
+}
