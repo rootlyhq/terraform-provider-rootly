82ef565a000f90546bb4ae54e983678d0b5e6cf7
Alex Mingoia <alex@alexmingoia.com>
2022-11-02 22:45:34 +0700

    Add form field option test.

---
+++ provider/resource_form_field_option_test.go
@@ -0,0 +1,57 @@
+package provider
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccResourceFormFieldOption(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceFormFieldOption,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("rootly_form_field.parent", "name", "myformfieldParent"),
+					resource.TestCheckResourceAttr("rootly_form_field_option.foo", "value", "myoption"),
+				),
+			},
+			{
+				Config: testAccResourceFormFieldOptionUpdate,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("rootly_form_field_option.foo", "value", "myoption2"),
+				),
+			},
+		},
+	})
+}
+
+const testAccResourceFormFieldOption = `
+resource "rootly_form_field" "parent" {
+  name = "myformfieldParent"
+	input_kind = "select"
+	shown = ["web_new_incident_form"]
+	required = ["web_new_incident_form"]
+}
+
+resource "rootly_form_field_option" "foo" {
+	form_field_id = rootly_form_field.parent.id
+  value = "myoption"
+}
+`
+
+const testAccResourceFormFieldOptionUpdate = `
+resource "rootly_form_field" "parent" {
+  name = "myformfieldParent"
+	input_kind = "select"
+	shown = ["web_new_incident_form"]
+	required = ["web_new_incident_form"]
+}
+
+resource "rootly_form_field_option" "foo" {
+	form_field_id = rootly_form_field.parent.id
+  value       = "myoption2"
+}
+`
