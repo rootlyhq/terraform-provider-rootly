8ebb7c09810af961ea5fae14f71eead63c36e960
Quentin Rousseau <contact@quent.in>
2023-03-15 16:43:27 -0700

    Improve rootly_workflow_incident example

--- examples/resources/rootly_workflow_incident/resource.tf
+++ examples/resources/rootly_workflow_incident/resource.tf
@@ -1,12 +1,18 @@
+data "rootly_severity" "critical" {
+  slug = "sev0"
+}
+
 resource "rootly_workflow_incident" "my-workflow" {
-  name        = "Trigger when an incident is created"
-  description = "This workflow will trigger when an incident is created"
+  name        = "Trigger when an incident is created and severity is critical"
+  description = "This workflow will trigger when an incident is created and severity is critical"
   trigger_params {
-    triggers                  = ["incident_created"]
-    incident_condition_kind   = "IS"
-    incident_kinds            = ["normal"]
-    incident_condition_status = "IS"
-    incident_statuses         = ["started"]
+    triggers                    = ["incident_created"]
+    incident_condition_kind     = "IS"
+    incident_kinds              = ["normal"]
+    incident_condition_status   = "IS"
+    incident_statuses           = ["started"]
+    incident_condition_severity = "IS"
   }
-  enabled = true
+  severity_ids = [data.rootly_severity.critical.id]
+  enabled      = true
 }
