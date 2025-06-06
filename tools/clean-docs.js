18c23efec39f7da8e1e37e35b1881c79aa14d822
Alexander Mingoia <alex@alexmingoia.com>
2025-06-06 10:26:31 -0700

    fix(docs): remove slug and fix syntax of import example
    Import blocks must specify `id`, they do not support identifying resources with other fields. Also adds missing `=` in examples.

--- tools/clean-docs.js
+++ tools/clean-docs.js
@@ -9,21 +9,15 @@ text = text.replace(
   "- `id` (String)"
 );
 
-// Check if the doc contains 'slug' (case-insensitive)
-const useSlug = /slug/i.test(text);
 // Replace import block
 text = text.replace(
   /import \{\s*\n\s*to ([^\n]+)\.primary\n\s*id = "[^"]+"\s*\n\}/,
-  useSlug
-    ? 'import {\n  to $1.my-resource\n  slug = "my-resource-slug"\n}'
-    : 'import {\n  to $1.my-resource\n  id = "00000000-0000-0000-0000-000000000000"\n}'
+  'import {\n  to $1.my-resource\n  id = "00000000-0000-0000-0000-000000000000"\n}'
 );
 // Replace terraform import command
 text = text.replace(
   /terraform import Resource\.([^\s]+) ([^\n]+)\n/,
-  useSlug
-    ? 'terraform import $1.my-resource my-resource-slug\n'
-    : 'terraform import $1.my-resource 00000000-0000-0000-0000-000000000000\n'
+  'terraform import $1.my-resource 00000000-0000-0000-0000-000000000000\n'
 );
 
 fs.writeFileSync(file, text, "utf-8");
