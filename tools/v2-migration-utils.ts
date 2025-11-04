5b6753a60ae5f6ad8f9715cb1c426fb2c25e97c9
Jian Yuan Lee <jianyuan@gmail.com>
2025-11-04 22:01:13 +0000

    feat: Fix dashboard_panel resource (#141)
    * feat: use typescript
    
    * fix: hasIncludeParam check
    
    * feat: re-enable codegen for dashboard_panel resource
    
    * feat: new internal/converter package
    
    * feat: v2 generator. re-enable codegen for dashboard_panel. add tests
    
    * feat: update docs
    
    * fix: nested required fields
    
    * feat: add upgrade test
    
    * rename files
    
    * fix: make dashboard_id required

---
+++ tools/v2-migration-utils.ts
@@ -0,0 +1,42 @@
+export const v2Resources = ["dashboard_panel"];
+
+type Schema = any;
+type SchemaMod = (schema: Schema) => Schema;
+
+export const SchemaMods: SchemaMod[] = [
+  function fixDashboardPanel(schema) {
+    // Converts {"oneOf": [{"type": "string"}, {"type": "object"}]} to {"type": "string"}
+    function fixDatasetsGroupByParam(paramsSchema: any) {
+      const datasetsItemsProperties =
+        paramsSchema.properties["datasets"].items.properties;
+      const groupBySchema = datasetsItemsProperties["group_by"];
+      const { oneOf, ...rest } = groupBySchema;
+      datasetsItemsProperties["group_by"] = {
+        ...oneOf[0],
+        ...rest,
+      };
+    }
+
+    const targets = [
+      schema.components.schemas["dashboard_panel"].properties["params"],
+      schema.components.schemas["new_dashboard_panel"].properties["data"]
+        .properties["attributes"].properties["params"],
+      schema.components.schemas["update_dashboard_panel"].properties["data"]
+        .properties["attributes"].properties["params"],
+    ];
+
+    for (const target of targets) {
+      fixDatasetsGroupByParam(target);
+    }
+
+    // Dashboard panel data is not saved to state
+    delete schema.components.schemas["dashboard_panel"].properties["data"];
+
+    // Dashboard panel requires dashboard_id
+    schema.components.schemas["new_dashboard_panel"].properties[
+      "data"
+    ].properties["attributes"].required.push("dashboard_id");
+
+    return schema;
+  },
+];
