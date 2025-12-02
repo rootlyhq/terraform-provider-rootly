68a8f22971e3a42e8c50f14c4f7f76c453b66f58
kwent <879780+kwent@users.noreply.github.com>
2025-12-02 21:39:47 +0000

    chore: update Terraform provider and generated code
    - Ran `make codegen` to regenerate resources and data sources.
    - Updated documentation via `make docs`.
    - Automated update to keep provider and generated documentation in sync.

--- provider/resource_workflow_task_publish_incident.go
+++ provider/resource_workflow_task_publish_incident.go
@@ -92,7 +92,7 @@ func resourceWorkflowTaskPublishIncident() *schema.Resource {
 							Optional:    true,
 						},
 						"status": &schema.Schema{
-							Description: "Value must be one of `investigating`, `identified`, `monitoring`, `resolved`, `planning`, `scheduled`, `in_progress`, `verifying`, `completed`, `cancelled`.",
+							Description: "Value must be one of `investigating`, `identified`, `monitoring`, `resolved`, `scheduled`, `in_progress`, `completed`.",
 							Type:        schema.TypeString,
 							Required:    true,
 							ValidateFunc: validation.StringInSlice([]string{
@@ -100,12 +100,9 @@ func resourceWorkflowTaskPublishIncident() *schema.Resource {
 								"identified",
 								"monitoring",
 								"resolved",
-								"planning",
 								"scheduled",
 								"in_progress",
-								"verifying",
 								"completed",
-								"cancelled",
 							}, false),
 						},
 						"notify_subscribers": &schema.Schema{
