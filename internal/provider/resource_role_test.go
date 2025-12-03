f0bd354fbf7d31bc8761a763d73df03c26811cfb
Jian Yuan Lee <jianyuan@gmail.com>
2025-12-03 16:45:20 +0000

    feat: Introduce base resource and data source configurations and centralize provider testing utilities.

--- internal/provider/resource_role_test.go
+++ internal/provider/resource_role_test.go
@@ -22,7 +22,7 @@ func TestAccResourceRole_UpgradeFromVersion(t *testing.T) {
 	}
 
 	resource.UnitTest(t, resource.TestCase{
-		PreCheck: func() { acctest.PreCheck(t) },
+		PreCheck: func() { testAccPreCheck(t) },
 		Steps: []resource.TestStep{
 			{
 				ExternalProviders: map[string]resource.ExternalProvider{
@@ -35,7 +35,7 @@ func TestAccResourceRole_UpgradeFromVersion(t *testing.T) {
 				ConfigStateChecks: configStateChecks,
 			},
 			{
-				ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
+				ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 				Config:                   testAccResourceRole(roleName),
 				ConfigStateChecks:        configStateChecks,
 			},
@@ -53,8 +53,8 @@ func TestAccResourceRole(t *testing.T) {
 	}
 
 	resource.UnitTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.PreCheck(t) },
-		ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccResourceRole(roleName),
