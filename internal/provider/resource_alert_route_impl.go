fa0fd7ed07c34bd24801b001e58cd39dd5ca938d
Jian Yuan Lee <jianyuan@gmail.com>
2025-12-04 12:37:11 +0000

    model to client model marshaller

--- internal/provider/resource_alert_route_impl.go
+++ internal/provider/resource_alert_route_impl.go
@@ -4,10 +4,10 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"net/http"
 
 	"github.com/DataDog/jsonapi"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
-	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/rootlyhq/terraform-provider-rootly/v2/internal/apiclient"
 )
 
@@ -17,19 +17,6 @@ func (r *AlertRouteResource) create(ctx context.Context, data *AlertRouteModel, 
 		resp.Diagnostics.AddError("ToClientModel Error", err.Error())
 		return
 	}
-
-	// modelIn := apiclient.AlertRouteModel{
-	// 	Name:    data.Name.ValueString(),
-	// 	Enabled: data.Enabled.ValueBool(),
-	// }
-
-	// if !data.AlertsSourceIds.IsNull() {
-	// 	modelIn.AlertsSourceIds = data.AlertsSourceIds.MustGet(ctx)
-	// }
-
-	// if !data.OwningTeamIds.IsNull() {
-	// 	modelIn.OwningTeamIds = data.OwningTeamIds.MustGet(ctx)
-	// }
 
 	b, err := jsonapi.Marshal(&modelIn, jsonapi.MarshalClientMode())
 	if err != nil {
@@ -55,7 +42,10 @@ func (r *AlertRouteResource) create(ctx context.Context, data *AlertRouteModel, 
 		return
 	}
 
-	data.Id = types.StringValue(modelOut.Id)
+	if err := FillAlertRouteModel(ctx, modelOut, data); err != nil {
+		resp.Diagnostics.AddError("FillModel Error", err.Error())
+		return
+	}
 }
 
 func (r *AlertRouteResource) read(ctx context.Context, data *AlertRouteModel, resp *resource.ReadResponse) {
@@ -66,11 +56,21 @@ func (r *AlertRouteResource) read(ctx context.Context, data *AlertRouteModel, re
 	} else if httpResp.StatusCode() < 200 || httpResp.StatusCode() >= 300 {
 		resp.Diagnostics.AddError("API Error", fmt.Sprintf("Unable to read, got status code: %d", httpResp.StatusCode()))
 		return
-	} else if httpResp.ApplicationvndApiJSON200 == nil {
+	} else if httpResp.Body == nil {
 		resp.Diagnostics.AddError("API Error", "Unable to read, got empty response")
 		return
 	}
 
+	var modelOut apiclient.AlertRouteModel
+	if err := jsonapi.Unmarshal(httpResp.Body, &modelOut); err != nil {
+		resp.Diagnostics.AddError("JSONAPI Unmarshal Error", err.Error())
+		return
+	}
+
+	if err := FillAlertRouteModel(ctx, modelOut, data); err != nil {
+		resp.Diagnostics.AddError("FillModel Error", err.Error())
+		return
+	}
 }
 
 func (r *AlertRouteResource) update(ctx context.Context, plan AlertRouteModel, data *AlertRouteModel, resp *resource.UpdateResponse) {
@@ -90,5 +90,17 @@ func (r *AlertRouteResource) update(ctx context.Context, plan AlertRouteModel, d
 }
 
 func (r *AlertRouteResource) delete(ctx context.Context, data *AlertRouteModel, resp *resource.DeleteResponse) {
-
+	httpResp, err := r.client.DeleteAlertRouteWithResponse(
+		ctx,
+		data.Id.ValueString(),
+	)
+	if err != nil {
+		resp.Diagnostics.AddError("API Error", err.Error())
+		return
+	} else if httpResp.StatusCode() == http.StatusNotFound {
+		return
+	} else if httpResp.StatusCode() < 200 || httpResp.StatusCode() >= 300 {
+		resp.Diagnostics.AddError("API Error", fmt.Sprintf("Unable to delete, got status code: %d", httpResp.StatusCode()))
+		return
+	}
 }
