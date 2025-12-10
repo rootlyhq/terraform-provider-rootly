7560d9b702fcdb9662ce866a669ddf66c7d6683e
Jian Yuan Lee <jianyuan@gmail.com>
2025-12-10 03:08:07 +0000

    generate escalation policy resource

--- internal/provider/resource_escalation_policy_test.go
+++ internal/provider/resource_escalation_policy_test.go
@@ -6,8 +6,12 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
 	"github.com/jianyuan/go-utils/ptr"
 	"github.com/rootlyhq/terraform-provider-rootly/v2/internal/acctest"
 	rootly "github.com/rootlyhq/terraform-provider-rootly/v2/schema"
@@ -56,4 +60,75 @@ func init() {
 			return nil
 		},
 	})
+}
+
+func TestAccResourceEscalationPolicy(t *testing.T) {
+	rn := "rootly_escalation_policy.test"
+	teamName := acctest.RandomWithPrefix("tf-team")
+	policyName := acctest.RandomWithPrefix("tf-policy")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceEscalationPolicyConfig(teamName, policyName),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(rn, tfjsonpath.New("id"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue(rn, tfjsonpath.New("name"), knownvalue.StringExact(policyName)),
+					statecheck.ExpectKnownValue(rn, tfjsonpath.New("business_hours"), knownvalue.ObjectExact(map[string]knownvalue.Check{
+						"days": knownvalue.SetExact([]knownvalue.Check{
+							knownvalue.StringExact("M"),
+							knownvalue.StringExact("T"),
+							knownvalue.StringExact("W"),
+							knownvalue.StringExact("R"),
+							knownvalue.StringExact("F"),
+						}),
+						"start_time": knownvalue.StringExact("09:00"),
+						"end_time":   knownvalue.StringExact("17:00"),
+						"time_zone":  knownvalue.Null(),
+					})),
+				},
+			},
+			{
+				Config: testAccResourceEscalationPolicyConfig(teamName, policyName+"-updated"),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(rn, tfjsonpath.New("id"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue(rn, tfjsonpath.New("name"), knownvalue.StringExact(policyName+"-updated")),
+					statecheck.ExpectKnownValue(rn, tfjsonpath.New("business_hours"), knownvalue.ObjectExact(map[string]knownvalue.Check{
+						"days": knownvalue.SetExact([]knownvalue.Check{
+							knownvalue.StringExact("M"),
+							knownvalue.StringExact("T"),
+							knownvalue.StringExact("W"),
+							knownvalue.StringExact("R"),
+							knownvalue.StringExact("F"),
+						}),
+						"start_time": knownvalue.StringExact("09:00"),
+						"end_time":   knownvalue.StringExact("17:00"),
+						"time_zone":  knownvalue.Null(),
+					})),
+				},
+			},
+		},
+	})
+}
+
+func testAccResourceEscalationPolicyConfig(teamName, policyName string) string {
+	return fmt.Sprintf(`
+resource "rootly_team" "test" {
+	name = "%s"
+}
+
+resource "rootly_escalation_policy" "test" {
+	name      = "%s"
+	group_ids = [rootly_team.test.id]
+
+	business_hours = {
+		days = ["M", "T", "W", "R", "F"]
+		start_time = "09:00"
+		end_time = "17:00"
+		time_zone = null
+	}
+}
+	`, teamName, policyName)
 }
