c09e66842fa464ab555b72e6fc4e2c5c4ad84572
Harneet Singh <Harneet@rootly.com>
2025-12-18 01:15:45 -0500

    (OC-2365): Add tests

--- provider/resource_service_test.go
+++ provider/resource_service_test.go
@@ -1,8 +1,10 @@
 package provider
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
@@ -56,3 +58,34 @@ resource "rootly_service" tf {
 	}
 }
 `
+
+func TestAccResourceServiceWithKubernetesDeploymentName(t *testing.T) {
+	service_name := acctest.RandomWithPrefix("tf-service")
+	kubernetes_deployment_name := "namespace/" + acctest.RandomWithPrefix("deployment-name")
+	tf_name := "tf-" + acctest.RandomWithPrefix("service")
+
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceServiceWithKubernetesDeploymentNameConfig(tf_name, service_name, kubernetes_deployment_name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("rootly_service.tf", "name", service_name),
+					resource.TestCheckResourceAttr("rootly_service.tf", "kubernetes_deployment_name", kubernetes_deployment_name),
+				),
+			},
+		},
+	})
+}
+
+func testAccResourceServiceWithKubernetesDeploymentNameConfig(tf_name, service_name, kubernetes_deployment_name string) string {
+	return fmt.Sprintf(`
+	resource "rootly_service" "%s" {
+		name = "%s"
+		kubernetes_deployment_name = "%s"
+	}
+	`, tf_name, service_name, kubernetes_deployment_name)
+}
