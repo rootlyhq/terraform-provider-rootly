880f11a3781c32344b09cab529893d74182dcdd9
Channi <Harneetchanni.singh@gmail.com>
2026-02-07 09:44:07 -0500

    [IR-4053] Add Missing Timezones to EscalationPaths (#218)
    * [IR-4056]: Workflow Task Update Resources
    
    * [IR-4056]: Add Docs
    
    * [IR-4056]: add workflow task update page resources (Confluence, Datadog, Dropbox, Quip, SharePoint)
    
    * [IR-4055]: Add IS NOT Parameter to Workflows
    
    * [IR-4055]: Fix oapi-config
    
    * [IR-4055]: Add IS NOT Parameter to Workflows
    
    * [IR-4055]: Add IS NOT Parameter to Workflows
    
    * [IR-4053]: Update Timezones
    
    * [IR-4053]: Fix Tests

--- provider/resource_form_set_test.go
+++ provider/resource_form_set_test.go
@@ -1,12 +1,16 @@
 package provider
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccResourceFormSet(t *testing.T) {
+	randomName := acctest.RandomWithPrefix("tf-test-form-set")
+
 	resource.UnitTest(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheck(t)
@@ -14,15 +18,20 @@ func TestAccResourceFormSet(t *testing.T) {
 		ProviderFactories: providerFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccResourceFormSet,
+				Config: testAccResourceFormSet(randomName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("rootly_form_set.test", "name", randomName),
+				),
 			},
 		},
 	})
 }
 
-const testAccResourceFormSet = `
+func testAccResourceFormSet(name string) string {
+	return fmt.Sprintf(`
 resource "rootly_form_set" "test" {
-	name = "test"
+	name = "%s"
 	forms = ["web_new_incident_form"]
 }
-`
+`, name)
+}
