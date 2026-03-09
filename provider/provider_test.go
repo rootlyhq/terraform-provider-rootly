52b0d0cd38e5faa0b726b6e30a44da3b04f0d5c2
Jian Yuan Lee <jianyuan@gmail.com>
2026-03-09 15:38:47 +0000

    feat: Add shared API client for acceptance tests and enhance heartbeat resource tests to verify heartbeat enabled status.

--- provider/provider_test.go
+++ provider/provider_test.go
@@ -1,9 +1,14 @@
 package provider
 
 import (
+	"log"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/rootlyhq/terraform-provider-rootly/v2/client"
+	"github.com/rootlyhq/terraform-provider-rootly/v2/meta"
+	rootly "github.com/rootlyhq/terraform-provider-rootly/v2/schema"
 )
 
 // providerFactories are used to instantiate a provider during acceptance testing.
@@ -25,4 +30,36 @@ func testAccPreCheck(t *testing.T) {
 	// You can add code here to run prior to any test case execution, for example assertions
 	// about the appropriate environment variables being set are common to see in a pre-check
 	// function.
+}
+
+var testAccSharedClient *rootly.ClientWithResponses
+
+func init() {
+	var apiHost string
+	if v := os.Getenv("ROOTLY_API_URL"); v != "" {
+		apiHost = v
+	} else {
+		apiHost = "https://api.rootly.com"
+	}
+
+	var apiToken string
+	if v := os.Getenv("ROOTLY_API_TOKEN"); v != "" {
+		apiToken = v
+	}
+
+	legacyClient, err := client.NewClient(apiHost, apiToken, RootlyUserAgent(meta.GetVersion()))
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	sharedClient, err := rootly.NewClientWithResponses(
+		apiHost,
+		// Piggyback on the legacy client's HTTP client. Inherits the same headers, authentication, and retry logic.
+		rootly.WithHTTPClient(legacyClient),
+	)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	testAccSharedClient = sharedClient
 }
