578bd7be5893fde3c960cff4b2a9735efd80ef5c
Harneet Singh <Harneet@rootly.com>
2026-03-10 20:34:10 -0400

    [OC-2750]: Add blank string to possible sev mapping for opsgenie

--- provider/resource_workflow_custom_field_selection_test.go
+++ provider/resource_workflow_custom_field_selection_test.go
@@ -1,18 +1,24 @@
 package provider
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccResourceWorkflowCustomFieldSelection(t *testing.T) {
+	workflowName := acctest.RandomWithPrefix("tf-workflow-custom-field")
+	field1Name := acctest.RandomWithPrefix("tf-custom-field-1")
+	field2Name := acctest.RandomWithPrefix("tf-custom-field-2")
+
 	resource.UnitTest(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t) },
 		ProviderFactories: providerFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccResourceWorkflowCustomFieldSelection,
+				Config: testAccResourceWorkflowCustomFieldSelection(workflowName, field1Name, field2Name),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("rootly_workflow_custom_field_selection.test1", "incident_condition", "ANY"),
 					resource.TestCheckResourceAttr("rootly_workflow_custom_field_selection.test2", "incident_condition", "ANY"),
@@ -22,20 +28,21 @@ func TestAccResourceWorkflowCustomFieldSelection(t *testing.T) {
 	})
 }
 
-const testAccResourceWorkflowCustomFieldSelection = `
+func testAccResourceWorkflowCustomFieldSelection(workflowName, field1Name, field2Name string) string {
+	return fmt.Sprintf(`
 resource "rootly_workflow_incident" "test" {
-  name = "workflow-custom-field-test"
+  name = "%s"
 }
 
 resource "rootly_custom_field" "test1" {
-  	label = "custom-field-test-1"
+	label = "%s"
 	kind = "select"
 	shown = ["incident_form", "incident_slack_form"]
 	required = []
 }
 
 resource "rootly_custom_field" "test2" {
-  	label = "custom-field-test-2"
+	label = "%s"
 	kind = "text"
 	shown = ["incident_form", "incident_slack_form"]
 	required = []
@@ -57,4 +64,5 @@ resource "rootly_workflow_custom_field_selection" "test2" {
 	custom_field_id = rootly_custom_field.test2.id
 	values = ["test"]
 }
-`
+`, workflowName, field1Name, field2Name)
+}
