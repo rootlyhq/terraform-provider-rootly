ac59b1b52de97559157a5096c1fc27d771dd5c76
Quentin Rousseau <contact@quent.in>
2026-05-04 22:48:46 -0700

    Fix data.rootly_services failing with Invalid address to set (#283)
    * Fix data.rootly_services failing with "Invalid address to set"
    
    structToLowerFirstMap converts all Service struct fields to a map, but
    the data source schema only defines 6 fields. Terraform rejects the
    extra keys. Add filterMapKeys helper to strip map entries not in schema.
    
    Fixes #257
    
    * Add all Service fields to data.rootly_services schema
    
    The data source only defined 6 fields (id, slug, name, color,
    description, public_description) but structToLowerFirstMap returns
    all 34 Service struct fields. Terraform rejected the extra fields
    with "Invalid address to set".
    
    Added all fields from the Service client struct to the data source
    schema. Kept filterMapKeys as safety net for any future fields the
    API adds that aren't yet in the schema.
    
    Fixes #257
    
    * Apply filterMapKeys to all plural data sources
    
    All 10 plural data sources use structToLowerFirstMap which returns
    every field from the client struct. If the struct has fields not in
    the data source schema, Terraform crashes with "Invalid address to
    set". This was the root cause of #257 for services.
    
    Apply the same filterMapKeys safety net to all 9 remaining plural
    data sources (causes, custom_field_options, custom_fields,
    environments, functionalities, incident_roles, incident_types,
    severities, teams). New API fields are silently filtered until the
    schema is updated, instead of crashing.

--- provider/data_source_services.go
+++ provider/data_source_services.go
@@ -40,30 +40,40 @@ func dataSourceServices() *schema.Resource {
 				Computed: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
-						"id": &schema.Schema{
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"slug": &schema.Schema{
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"name": &schema.Schema{
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"color": &schema.Schema{
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"description": &schema.Schema{
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"public_description": &schema.Schema{
-							Type:     schema.TypeString,
-							Computed: true,
-						},
+						"id": {Type: schema.TypeString, Computed: true},
+						"name": {Type: schema.TypeString, Computed: true},
+						"slug": {Type: schema.TypeString, Computed: true},
+						"description": {Type: schema.TypeString, Computed: true},
+						"public_description": {Type: schema.TypeString, Computed: true},
+						"color": {Type: schema.TypeString, Computed: true},
+						"position": {Type: schema.TypeInt, Computed: true},
+						"backstage_id": {Type: schema.TypeString, Computed: true},
+						"external_id": {Type: schema.TypeString, Computed: true},
+						"pagerduty_id": {Type: schema.TypeString, Computed: true},
+						"opsgenie_id": {Type: schema.TypeString, Computed: true},
+						"cortex_id": {Type: schema.TypeString, Computed: true},
+						"service_now_ci_sys_id": {Type: schema.TypeString, Computed: true},
+						"github_repository_name": {Type: schema.TypeString, Computed: true},
+						"github_repository_branch": {Type: schema.TypeString, Computed: true},
+						"gitlab_repository_name": {Type: schema.TypeString, Computed: true},
+						"gitlab_repository_branch": {Type: schema.TypeString, Computed: true},
+						"kubernetes_deployment_name": {Type: schema.TypeString, Computed: true},
+						"notify_emails": {Type: schema.TypeList, Computed: true, Elem: &schema.Schema{Type: schema.TypeString}},
+						"environment_ids": {Type: schema.TypeList, Computed: true, Elem: &schema.Schema{Type: schema.TypeString}},
+						"service_ids": {Type: schema.TypeList, Computed: true, Elem: &schema.Schema{Type: schema.TypeString}},
+						"owner_group_ids": {Type: schema.TypeList, Computed: true, Elem: &schema.Schema{Type: schema.TypeString}},
+						"owner_user_ids": {Type: schema.TypeList, Computed: true, Elem: &schema.Schema{Type: schema.TypeString}},
+						"alert_urgency_id": {Type: schema.TypeString, Computed: true},
+						"escalation_policy_id": {Type: schema.TypeString, Computed: true},
+						"alerts_email_enabled": {Type: schema.TypeBool, Computed: true},
+						"alerts_email_address": {Type: schema.TypeString, Computed: true},
+						"slack_channels": {Type: schema.TypeList, Computed: true, Elem: &schema.Schema{Type: schema.TypeString}},
+						"slack_aliases": {Type: schema.TypeList, Computed: true, Elem: &schema.Schema{Type: schema.TypeString}},
+						"alert_broadcast_enabled": {Type: schema.TypeBool, Computed: true},
+						"alert_broadcast_channel": {Type: schema.TypeMap, Computed: true, Elem: &schema.Schema{Type: schema.TypeString}},
+						"incident_broadcast_enabled": {Type: schema.TypeBool, Computed: true},
+						"incident_broadcast_channel": {Type: schema.TypeMap, Computed: true, Elem: &schema.Schema{Type: schema.TypeString}},
+						"properties": {Type: schema.TypeList, Computed: true, Elem: &schema.Schema{Type: schema.TypeString}},
 					},
 				},
 			},
@@ -87,10 +97,11 @@ func dataSourceServicesRead(ctx context.Context, d *schema.ResourceData, meta in
 		return diag.FromErr(err)
 	}
 
+	serviceSchema := dataSourceServices().Schema["services"].Elem.(*schema.Resource).Schema
 	tf_services := make([]interface{}, len(services), len(services))
 	for i, service := range services {
 		c, _ := service.(*client.Service)
-		tf_services[i] = structToLowerFirstMap(*c)
+		tf_services[i] = filterMapKeys(structToLowerFirstMap(*c), serviceSchema)
 	}
 
 	if err := d.Set("services", tf_services); err != nil {
