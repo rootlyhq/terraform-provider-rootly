a556fcef48aa9e47cc20b655fbc7b64302b40b84
Quentin Rousseau <contact@quent.in>
2026-07-29 20:45:03 -0700

    Fix CreateMistralChatCompletion test: set max_tokens to valid value
    API validates max_tokens > 0 (minimum: 1 in swagger) but field is
    optional. Test was sending implicit 0. Hand-maintain with max_tokens=100.

--- provider/resource_workflow_task_create_mistral_chat_completion_test.go
+++ provider/resource_workflow_task_create_mistral_chat_completion_test.go
@@ -1,9 +1,49 @@
 package provider
 
-// Skipped: API requires max_tokens > 0 but swagger doesn't mark it required.
+import (
+	"fmt"
+	"testing"
 
-import "testing"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
 
 func TestAccResourceWorkflowTaskCreateMistralChatCompletion(t *testing.T) {
-	t.Skip("API requires max_tokens > 0 but field is not required in schema")
+	rName := acctest.RandomWithPrefix("tf-wf-mistral")
+
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceWorkflowTaskCreateMistralChatCompletionConfig(rName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("rootly_workflow_task_create_mistral_chat_completion.foo", "enabled", "true"),
+					resource.TestCheckResourceAttr("rootly_workflow_task_create_mistral_chat_completion.foo", "task_params.0.task_type", "create_mistral_chat_completion"),
+				),
+			},
+		},
+	})
+}
+
+func testAccResourceWorkflowTaskCreateMistralChatCompletionConfig(name string) string {
+	return fmt.Sprintf(`
+resource "rootly_workflow_incident" "foo" {
+  name = "%s"
+  trigger_params {
+    triggers = ["incident_created"]
+  }
+}
+
+resource "rootly_workflow_task_create_mistral_chat_completion" "foo" {
+  workflow_id = rootly_workflow_incident.foo.id
+  task_params {
+    model      = "mistral-small-latest"
+    prompt     = "test prompt"
+    max_tokens = 100
+  }
+}
+`, name)
 }
