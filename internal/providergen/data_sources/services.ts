699c2270f12f6a2da1fdf5f78ee0426d2b864cf2
Jian Yuan Lee <jianyuan@gmail.com>
2026-09-10 16:39:39 +0100

    refactor: implement custom helpers for JSONAPI type conversions

--- internal/providergen/data_sources/services.ts
+++
@@ -1,8 +0,0 @@
-import type { DataSourceConfig } from "../schema";
-
-export default {
-  name: "services",
-  type: "list",
-  resourceName: "service",
-  description: "Retrieves a list of all services.",
-} satisfies DataSourceConfig;
