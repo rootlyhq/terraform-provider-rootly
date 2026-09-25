29174c57cff4b80e422d77cf197cdf1501af19f0
Jian Yuan Lee <jianyuan@gmail.com>
2026-09-25 02:19:22 +0100

    fix: unsetting alert broadcast configuration on service resource

--- client/services.go
+++ client/services.go
@@ -142,11 +142,10 @@ func (c *Client) DeleteService(id string) error {
 		return fmt.Errorf("Error building request: %w", err)
 	}
 
-	resp, err := c.Do(req)
+	_, err = c.Do(req)
 	if err != nil {
 		return fmt.Errorf("Failed to make request to delete service: %w", err)
 	}
-	resp.Body.Close()
 
 	return nil
 }
